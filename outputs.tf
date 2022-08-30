07478e68dc1acb2c63ba1e61bc4f1a82354aed64
Paul Dwerryhouse <paul@dwerryhouse.com.au>
2022-08-30 20:45:08 +1000

    add licence notices

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
+##    terraform-azure-linux-vm
+##    Copyright (C) 2022 Paul Dwerryhouse <paul@dwerryhouse.com.au>
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 output "id" { value = azurerm_linux_virtual_machine.instance.id }
 output "public_ip_address" { value = azurerm_linux_virtual_machine.instance.public_ip_address }
 output "private_ip_address" { value = azurerm_linux_virtual_machine.instance.private_ip_address }
